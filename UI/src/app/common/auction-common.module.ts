b3d4a5d36c1bf40acf20dddaaf501cc3b8250449
Krzysztof Góral <KrzysztofPrawdziwyGoral@wp.pl>
2023-04-20 10:55:38 +0200

    xascads

--- UI/src/app/common/auction-common.module.ts
+++ UI/src/app/common/auction-common.module.ts
@@ -1,18 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { LogoComponent } from "src/app/common/components/logo/logo.component";
 import { MessageComponent } from "src/app/common/components/message/message.component";
 
 @NgModule({
   declarations: [
-    LogoComponent,
     MessageComponent,
   ],
   imports: [
     CommonModule,
   ],
   exports: [
-    LogoComponent,
     MessageComponent,
   ]
 })
